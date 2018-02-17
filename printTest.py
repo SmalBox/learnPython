783f2d941febf10c3daa03ccc20396555af14c2d
zcy-s1 <505414969@qq.com>
2018-02-18 01:43:35 +0800

    add printTest.py and update pythonNote.txt for string

---
+++ printTest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+n = 123
+f = 456.789
+s1 = 'hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print('字符串测试')
+print('n:',n,'\n')
+print('f:',f,'\n')
+print('s1:',s1,'\n')
+print('s2:',s2,'\n')
+print('s3:',s3,'\n')
+print('s4:',s4,'\n')
+
